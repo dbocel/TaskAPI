007ca17a6e34ad52f71ed3e918058bc464875bec
dbocel <dbocelj@miumg.edu.gt>
2025-01-21 15:52:03 -0600

    Reinstalando SQLite3 y ajustando el entorno

--- database.js
+++ database.js
@@ -1,24 +1,4 @@
-const sqlite3 = require('sqlite3').verbose();
-
-// Crear la base de datos
-const db = new sqlite3.Database('./tasks.db', (err) => {
-  if (err) {
-    console.error('Error al conectar con la base de datos:', err.message);
-  } else {
-    console.log('Conexión exitosa a la base de datos SQLite.');
-  }
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
-
-// Crear la tabla si no existe
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS tasks (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      title TEXT NOT NULL,
-      description TEXT,
-      dueDate TEXT
-    )
-  `);
-});
-
-module.exports = db;
